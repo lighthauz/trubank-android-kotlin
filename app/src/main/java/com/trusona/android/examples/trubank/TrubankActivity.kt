967b5b724ff3e3bb4a1fa8c7db08a2baf4b9ac1d
jd <juandiegogarita@gmail.com>
2018-11-26 17:33:53 -0600

    Updates activity

--- app/src/main/java/com/trusona/android/examples/trubank/TrubankActivity.kt
+++ app/src/main/java/com/trusona/android/examples/trubank/TrubankActivity.kt
@@ -1,21 +1,58 @@
 package com.trusona.android.examples.trubank
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-
-import kotlinx.android.synthetic.main.activity_splash.*
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.trusona.android.registration.device.DeviceStatus
+import kotlinx.android.synthetic.main.activity_trubank.*
 
 class TrubankActivity : AppCompatActivity() {
 
+    val mainContent = R.id.main_content
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_trubank)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        getDeviceIdentifier()
+    }
+
+    fun showGenericErrorDialog() {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setOnDismissListener(DialogInterface::dismiss)
+        dialog.setTitle("Error")
+        dialog.setMessage("Trusona Service")
+        dialog.show()
+    }
+
+    fun getDeviceIdentifier() {
+        TrubankCore.initTrusonaCredentials()
+        TrubankCore.trusona!!.getDeviceIdentifier(this) { response ->
+            Log.d("TrubankActivity", response.deviceStatus.name)
+
+            when (response.deviceStatus) {
+
+                DeviceStatus.ACTIVE_DEVICE -> supportFragmentManager.beginTransaction().replace(
+                    mainContent,
+                    HomeFragment()
+                ).commit()
+
+                DeviceStatus.INACTIVE_DEVICE,
+                DeviceStatus.NEW_DEVICE ->
+                    supportFragmentManager.beginTransaction().replace(
+                        mainContent,
+                        RegistrationFragment.newInstance(response.deviceIdentifier!!)
+                    ).commit()
+
+                DeviceStatus.INVALID_CREDENTIALS,
+                DeviceStatus.INVALID_DEVICE,
+                DeviceStatus.INVALID_RELYING_PARTY,
+                DeviceStatus.SERVER_ERROR,
+                DeviceStatus.UNKNOWN -> showGenericErrorDialog()
+            }
         }
     }
 
