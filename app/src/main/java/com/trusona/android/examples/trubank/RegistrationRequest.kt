d52bb164a43033d00063c260f5fb0174ea237239
JD <juandiegogarita@gmail.com>
2019-05-13 10:15:48 -0600

    delete old project

--- app/src/main/java/com/trusona/android/examples/trubank/RegistrationRequest.kt
+++
@@ -1,3 +0,0 @@
-package com.trusona.android.examples.trubank
-
-data class RegistrationRequest(val deviceIdentifier: String, val emailAddress: String)
--- app/src/main/java/com/trusona/android/examples/trubank/RegistrationResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.trusona.android.examples.trubank
-
-data class RegistrationResponse(val id: String, val emailAddress: String, val expiredAt: String, val isCompleted : Boolean)
